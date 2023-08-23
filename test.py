bf2ad7a8a695f15997168e192ebfe9de088de4dc
Valijonzoda <valijonzodafirdavs@gmail.com>
2023-08-23 11:29:32 +0500

    remove comment

--- test.py
+++ test.py
@@ -1,23 +1,19 @@
 import logging
 import json
-import asyncio
-from datetime import datetime, timedelta
+from datetime import datetime
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import ParseMode, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.types import ParseMode, ReplyKeyboardMarkup, KeyboardButton
 
 TOKEN = '6562742616:AAEahEb3TdScf5jSqtsFxQVdsMU8SufZrbc'
 
-
-# Initialize bot and dispatcher
+# Инициализация бота и диспетчера
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
-dp.middleware.setup(LoggingMiddleware())
-
-messages_to_delete = {}
+logging.basicConfig(level=logging.INFO)
 
 DATA_FILE = "savings_data.json"
 
+# Загрузка данных из файла
 try:
     with open(DATA_FILE, 'r') as f:
         user_data = json.load(f)
@@ -25,6 +21,7 @@ except (FileNotFoundError, json.JSONDecodeError):
     user_data = {}
 
 def save_data_to_file():
+    """Сохраняет данные в файл."""
     with open(DATA_FILE, 'w') as f:
         json.dump(user_data, f)
 
@@ -42,27 +39,12 @@ keyboard.add(
     KeyboardButton("Обнулить")
 )
 
-inline_keyboard = InlineKeyboardMarkup().add(InlineKeyboardButton("Добавить", callback_data="add_comment"))
-
-awaiting_comment_users = {}
-awaiting_sum_users = {}
-
-async def delete_messages_after_delay(user_id, delay):
-    await asyncio.sleep(delay)
-    if user_id in messages_to_delete:
-        for message_id in messages_to_delete[user_id]:
-            try:
-                await bot.delete_message(user_id, message_id)
-            except Exception as e:
-                logging.error(f"Error deleting message: {e}")
-        del messages_to_delete[user_id]
-
-
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     await message.answer('Пожалуйста, отправьте сумму, которую вы хотите добавить.', reply_markup=keyboard)
 
 def is_integer(value):
+    """Проверяет, является ли значение целым числом."""
     try:
         int(value)
         return True
@@ -78,16 +60,14 @@ async def handle_preset_values(message: types.Message):
     if user_id not in user_data:
         user_data[user_id] = []
 
-    user_data[user_id].append({"amount": number, "date": current_date, "comment": ""})
+    user_data[user_id].append({"amount": number, "date": current_date})
     save_data_to_file()
 
     total = sum(item["amount"] for item in user_data[user_id])
-    awaiting_sum_users[user_id] = True
-    await message.answer(f"Успешно добавлена! Текущая сумма: {total}c")
-    await message.answer("Введите комментарий:", reply_markup=inline_keyboard)
+    await message.answer(f"Успешно добавлена! Текущая сумма: {total}c", reply_markup=keyboard)
 
-@dp.message_handler(lambda message: is_integer(message.text) and str(message.from_user.id) not in awaiting_comment_users)
-async def sum_numbers(message: types.Message):
+@dp.message_handler(lambda message: is_integer(message.text))
+async def handle_custom_value(message: types.Message):
     user_id = str(message.from_user.id)
     number = int(message.text)
     current_date = datetime.now().strftime("%d-%m-%Y")
@@ -95,75 +75,21 @@ async def sum_numbers(message: types.Message):
     if user_id not in user_data:
         user_data[user_id] = []
 
-    user_data[user_id].append({"amount": number, "date": current_date, "comment": ""})
+    user_data[user_id].append({"amount": number, "date": current_date})
     save_data_to_file()
 
     total = sum(item["amount"] for item in user_data[user_id])
-    awaiting_sum_users[user_id] = True
-    await message.answer(f"Текущая сумма: {total}c\n\nВведите комментарий:", reply_markup=inline_keyboard)
-
-@dp.message_handler(lambda message: str(message.from_user.id) in awaiting_sum_users)
-async def handle_comment(message: types.Message):
-    user_id = str(message.from_user.id)
-    user_data[user_id][-1]["comment"] = message.text
-    save_data_to_file()
-
-    del awaiting_sum_users[user_id]
-    sent_msg = await message.answer("Комментарий добавлен!", reply_markup=keyboard)
-    if user_id not in messages_to_delete:
-    	messages_to_delete[user_id] = []
-    messages_to_delete[user_id].append(sent_msg.message_id)
-
-
-@dp.callback_query_handler(lambda c: c.data == 'add_comment')
-async def process_callback(callback_query: types.CallbackQuery):
-    await bot.answer_callback_query(callback_query.id)
-    awaiting_sum_users[str(callback_query.from_user.id)] = True
-    sent_msg = await bot.send_message(callback_query.from_user.id, "Введите ваш комментарий:")
-    await delete_messages_after_delay(user_id, 10)
-
-    asyncio.create_task(delete_message_after_delay(sent_msg.chat.id, sent_msg.message_id, 10))
-
+    await message.answer(f"Текущая сумма: {total}c", reply_markup=keyboard)
 
 @dp.message_handler(lambda message: message.text in ['Показать сбережения', 'Неделя', 'Месяц', 'Обнулить'])
 async def handle_buttons(message: types.Message):
-    user_id = str(message.from_user.id)
-    today = datetime.now()
-    action = message.text
+    # ... [Оставить этот код без изменений]
 
-    if action == 'Неделя':
-        end_date = today
-        start_date = today - timedelta(days=7)
-        title = "<i>За неделю</i>"
-    elif action == 'Месяц':
-        end_date = today
-        start_date = today - timedelta(days=30)
-        title = "<i>За месяц</i>"
-    elif action == 'Обнулить':
-        user_data[user_id] = []
-        save_data_to_file()
-        await message.answer("Ваши сбережения были обнулены.", reply_markup=keyboard)
-        return
-    else:
-        start_date = None
-        end_date = None
-        title = "<b>Экономия за всё время:</b>"
-
-    if user_id not in user_data or not user_data[user_id]:
-        await message.answer("У вас пока нет сбережений.", reply_markup=keyboard)
-        return
-
-    infotext = "\n<b>№   Сумма        Дата       Комментарий</b>\n"
-    savings_list = [f"<i>{idx + 1})</i> <b>    {item['amount']}</b>{' ' * (15 - len(str(item['amount'])))}   {item['date'][:5]}{' ' * (10 - len(item['date'][:5]))}     {item.get('comment', '')}" 
-                    for idx, item in enumerate(user_data[user_id]) 
-                    if not start_date or (datetime.strptime(item['date'], "%d-%m-%Y") >= start_date and datetime.strptime(item['date'], "%d-%m-%Y") <= end_date)]
-    total_sum = sum(item["amount"] for item in user_data[user_id] if not start_date or (datetime.strptime(item['date'], "%d-%m-%Y") >= start_date and datetime.strptime(item['date'], "%d-%m-%Y") <= end_date))
-    text = title + infotext + "\n".join(savings_list) + f"\n\nИтого: <b>{total_sum}</b><i>cомон</i>"
-    await message.answer(text, reply_markup=keyboard, parse_mode=ParseMode.HTML)
-@dp.message_handler(lambda message: not is_integer(message.text) and str(message.from_user.id) not in awaiting_sum_users)
+@dp.message_handler(lambda message: not is_integer(message.text))
 async def non_integer_message(message: types.Message):
     await message.answer("Введите только целые числа.", reply_markup=keyboard)
 
 if __name__ == "__main__":
     from aiogram import executor
     executor.start_polling(dp, skip_updates=True)
+
